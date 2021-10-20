980c9ef12a98a1de8be762d3c0eb873def4ebb3c
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-10-20 06:58:21 -0800

    WIP Titles do not toggle off

--- js/scripts.js
+++ js/scripts.js
@@ -9,4 +9,15 @@ $(document).ready(function() {
     $("#OperatorstTitle").toggle();
     $("#OperatorsDefinition").toggle();
   });
+
+  $(".Variables").click(function() {
+    $("#VariablestTitle").toggle();
+    $("#VariablesDefinition").toggle();
+  });
+
+  $(".VariableNamingConventions").click(function() {
+    $("#VariableNamingConventionsTitle").toggle();
+    $("#VariableNamingConventionsDefinition").toggle();
+  });
+
 });
