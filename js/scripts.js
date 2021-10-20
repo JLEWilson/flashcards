e7c9f61d5d10091a398f7f36adc0855413ccb1d9
Jacob Wilson <jacobleeeugenewilson@gmail.com>
2021-10-20 06:43:50 -0800

    Added click function

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+  
+  $(".JavaScript").click(function() {  
+    $("#JavaScriptTitle").toggle();
+    $("#JavaScriptDefinition").toggle();
+  });
+    
+  $(".Operators").click(function() {
+    $("#OperatorstTitle").toggle();
+    $("#OperatorsDefinition").toggle();
+  });
+});
